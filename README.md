24c1a7c7adc85867301864b037417a269a1b6859
Doug Davidoff <douglass.davidoff@gmail.com>
2020-10-14 18:14:09 -0400

    Testing license statements

--- README.md
+++ README.md
@@ -1,30 +1,41 @@
-# a
-## b
- ![License: MIT]() 
+---FastCompany.com says, '[E]xtremely straightforward and open. It permits users to do anything with a given project as long as they credit the developer and don’t hold him or her liable for the project’s use.'---
+
+
+# Beautify Bridgeport
+## Doug Davidoff
+ ![License: MIT](http://img.shields.io/static/v1?label=License&message=MIT&color=orange) 
 ### Table of Contents
 [Project Description](###project-description)
+
 [Installation](###installation)
+
 [Usage](###usage)
+
 [Future Development](###future-development)
+
 [Contributors](###contributors)
+
 [Tests](###tests)
+
 [License](###license)
+
 [Questions, Contact Info](###questions-and-contact-info)
+
 ### Project Description
- e
+ Planty flowers everywhere and knock down some old factories
 ### Installation
- f
+ bring a bulldozewr and a shovel
 ### Usage
- g
+ buy a lot of flower bulbs
 ### Future Development
- h
+ Keep on keeping on
 ### Contributors
- j
+ Bridgeporters
 ### Tests
- k
-### License
- 
+ We beautified downtown 
+ ###License
+ FastCompany.com says, '[E]xtremely straightforward and open. It permits users to do anything with a given project as long as they credit the developer and don’t hold him or her liable for the project’s use.'
 ### Questions and Contact Info
- l
- * Link to Github profile for the developer, b: [GitHub](https://github.com/c)
- * Email address for b: d
+ Contact the developer.
+ * Link to Github profile for the developer, Doug Davidoff: [GitHub](https://github.com/dougdavidoff)
+ * Email address for Doug Davidoff: doug@mail.com
--- index.js
+++ index.js
@@ -75,29 +75,29 @@ function promptUser() {
 
 function generateMD(content) {
     console.log(content);
-    switch ("licenseDescription") {
-        case license = "MIT":
+    switch (content.license) {
+        case "MIT":
             describeLicense = "FastCompany.com says, '[E]xtremely straightforward and open. It permits users to do anything with a given project as long as they credit the developer and don’t hold him or her liable for the project’s use.'";
             urlLicense = "http://img.shields.io/static/v1?label=License&message=MIT&color=orange";
             break;
-        case license = "Apache 2.0":
+        case "Apache 2.0":
             describeLicense = "FastCompany.com says, '[S]imilar to the MIT License, but also explicitly grants patent rights to users.'";
             urlLicense = "http://img.shields.io/static/v1?label=License&message=Apache&color=orange";            
             break;
-        case license = "GPLv3":
+        case "GPLv3":
             describeLicense = "FastCompany.com says, '[O]lder, more limiting, and less popular than [MIT or Apache]. It is a copyleft license that requires users to track their changes if they modify and then distribute a project. Different versions of this license also restrict the use of modified code in various classes of hardware.'";
             urlLicense = "http://img.shields.io/static/v1?label=License&message=GPL&color=orange";
             break;
-        case license = "AGPLv3":
+        case "AGPLv3":
             describeLicense = "GNU.org says, 'The GNU Affero General Public License is a free, copyleft license for software and other kinds of works, specifically designed to ensure cooperation with the community in the case of network server software. The licenses for most software and other practical works are designed to take away your freedom to share and change the works. By contrast, our General Public Licenses are intended to guarantee your freedom to share and change all versions of a program--to make sure it remains free software for all its users.'";
             urlLicense = "http://img.shields.io/static/v1?label=License&message=AGPL&color=orange";
             break;
-        case license = "wtfpl":
+        case "wtfpl":
             describeLicense = "github.com/dtf0 says, 'Ok, the purpose of this license is simple and you just do what the f**k you want to.'";
             urlLicense = "http://img.shields.io/static/v1?label=License&message=wtfplL&color=orange";
             break;
     }
-    return `# ${content.projectTitle}\n## ${content.developer}\n ![License: ${content.license}](${urlLicense}) \n### Table of Contents\n[Project Description](###project-description)\n[Installation](###installation)\n[Usage](###usage)\n[Future Development](###future-development)\n[Contributors](###contributors)\n[Tests](###tests)\n[License](###license)\n[Questions, Contact Info](###questions-and-contact-info)\n### Project Description\n ${content.description}\n### Installation\n ${content.installation}\n### Usage\n ${content.usage}\n### Future Development\n ${content.future}\n### Contributors\n ${content.contributing}\n### Tests\n ${content.tests}\n### License\n ${describeLicense}\n### Questions and Contact Info\n ${content.questions}\n * Link to Github profile for the developer, ${content.developer}: [GitHub](https://github.com/${content.github})\n * Email address for ${content.developer}: ${content.email}`
+    return `# ${content.projectTitle}\n## ${content.developer}\n ![License: ${content.license}](${urlLicense}) \n### Table of Contents\n[Project Description](###project-description)\n\n[Installation](###installation)\n\n[Usage](###usage)\n\n[Future Development](###future-development)\n\n[Contributors](###contributors)\n\n[Tests](###tests)\n\n[License](###license)\n\n[Questions, Contact Info](###questions-and-contact-info)\n\n### Project Description\n ${content.description}\n### Installation\n ${content.installation}\n### Usage\n ${content.usage}\n### Future Development\n ${content.future}\n### Contributors\n ${content.contributing}\n### Tests\n ${content.tests}\n ###License\n ${describeLicense}\n### Questions and Contact Info\n ${content.questions}\n * Link to Github profile for the developer, ${content.developer}: [GitHub](https://github.com/${content.github})\n * Email address for ${content.developer}: ${content.email}`
 }
 
 async function init() {
